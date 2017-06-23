f359a6a43b261eb6ccbfdc95ac446a80c52b7e93
Mohammad Mohibbullah <mbmohib@gmail.com>
2017-06-24 02:26:09 +0600

    portfolio sec finished

--- js/master.js
+++ js/master.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+    
+/*----------------------------------------*/
+/* Typewriter */
+/*----------------------------------------*/
     $(".print").typed({
         strings: [
           "I am Mohib", 
@@ -11,10 +15,60 @@ $(document).ready(function() {
         backDelay: 900,
     });
     
-   
+/*----------------------------------------*/
+/* Skills */
+/*----------------------------------------*/
+    
     $('.skillbar').each(function(){
 		$(this).find('.skillbar-bar').animate({
 			width:$(this).attr('data-percent')
 		}, 5000);
 	});
-});
+    
+/*----------------------------------------*/
+/* Isotope */
+/*----------------------------------------*/
+    
+
+    /* activate jquery isotope */
+    var $container = $('#posts').isotope({
+        itemSelector : '.item',
+        isFitWidth: true,
+        filter: '*'
+    });
+    
+    $container.imagesLoaded().progress( function() {
+        $container.isotope('layout');
+    });
+
+
+
+    
+
+    // filter items on button click
+    $('#filters').on( 'click', 'button', function() {
+        var filterValue = $(this).attr('data-filter');
+        $container.isotope({ filter: filterValue });
+    });
+    
+    //Toggle image overlay
+    var itemWrap = $('.item-wrap');
+    
+    itemWrap.on('mouseover', function() {
+        $(this).find('.img-overlay').addClass('animated zoomOutDown');
+    });
+    
+    itemWrap.on('mouseout', function() {
+        $(this).find('.img-overlay')
+            .removeClass('animated zoomOutDown')
+            .addClass('animated zoomInUp');
+    });
+
+
+
+    
+}); //Document ready function end
+
+
+
+
