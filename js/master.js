256b5f37dd3914baf6f65f3e7ef6410232ec6280
Mohammad Mohibbullah <mbmohib@gmail.com>
2017-06-25 03:36:21 +0600

    add wow, waypoint js, sticky nav

--- js/master.js
+++ js/master.js
@@ -42,9 +42,6 @@ $(document).ready(function() {
     });
 
 
-
-    
-
     // filter items on button click
     $('#filters').on( 'click', 'button', function() {
         var filterValue = $(this).attr('data-filter');
@@ -64,9 +61,75 @@ $(document).ready(function() {
             .addClass('animated zoomInUp');
     });
 
-
-
+/*----------------------------------------*/
+/* Initialize WOW */
+/*----------------------------------------*/
     
+new WOW().init();
+    
+/*----------------------------------------*/
+/* Sticky Nav */
+/*----------------------------------------*/
+    $('#about').waypoint(function(direction) {
+        if(direction == 'down') {
+            $('header').addClass('sticky');
+        } else {
+            $('header').removeClass('sticky');
+        }
+    }, {
+        offset : '80px'
+    });
+    
+    
+    
+    $('.section-header').waypoint(function(direction) {
+        if(direction == 'down') {
+            $(this).addClass('js');
+        }
+        
+    }, {
+        offset : '80%'
+    });
+    
+    
+/*----------------------------------------*/
+/* Smooth Scrolling */
+/*----------------------------------------*/   
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 }); //Document ready function end
 
 
